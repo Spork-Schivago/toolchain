ef2bc91b46beeae376946d471b52f774036ebb1c
Spork Schivago <SporkSchivago@gmail.com>
2014-06-24 00:08:58 -0400

    Added a path to check-gmp.sh dependency checker script.

--- depends/check-gmp.sh
+++ depends/check-gmp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # check-gmp.sh by Timothy Redaelli (timothy@redaelli.eu)
 
-[ -f /usr/include/gmp.h -o -f /opt/local/include/gmp.h -o -f /usr/local/include/gmp.h -o -f /opt/csw/include/gmp.h ] || { echo "ERROR: Install gmp before continuing."; exit 1; }
+[ -f /usr/include/gmp.h -o -f /usr/include/x86_64-linux-gnu/gmp.h -o -f /opt/local/include/gmp.h -o -f /usr/local/include/gmp.h -o -f /opt/csw/include/gmp.h ] || { echo "ERROR: Install gmp-devel before continuing."; exit 1; }
