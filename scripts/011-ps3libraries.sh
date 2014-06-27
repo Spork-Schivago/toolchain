cb46b3f242808f01ce63f7f63cf2ce39def0e30b
Spork Schivago <SporkSchivago@gmail.com>
2014-06-27 01:48:51 -0400

    Fixed a small typo

--- scripts/011-ps3libraries.sh
+++ scripts/011-ps3libraries.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 # 011-ps3libraries.sh by Spork Schivago (SporkSchivago@gmail.com)
 
-PS3LIBS = "PS3Libraries"
+PS3LIBS="PS3Libraries"
 
 ## Clone the source code from our github repository
 git clone https://github.com/Spork-Schivago/${PS3LIBS}
