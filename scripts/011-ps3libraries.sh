89636253f7fc4bc833a8e716521c0eae8011fcb7
Spork Schivago <SporkSchivago@gmail.com>
2014-06-26 23:52:27 -0400

    Modified the script to now download the modified PS3Libraries from our repository.

--- scripts/011-ps3libraries.sh
+++ scripts/011-ps3libraries.sh
@@ -1,21 +1,10 @@
 #!/bin/sh -e
-# SDL2_libs.sh by Dan Peori (dan.peori@oopo.net) (Updated by Spork Schivago)
+# 011-ps3libraries.sh by Spork Schivago (SporkSchivago@gmail.com)
 
-PS3LIBS="ps3libraries"
+PS3LIBS = "PS3Libraries"
 
-## Check for the source code in the previous directory
-## because we have no place to host it {:-(
-if [ ! -f ../${PS3LIBS}.tar.xz ]; then
-  echo "This distribution must have the ${PS3LIBS}.tar.xz file in the root directory of the toolchain...";
-  exit 1
-else
-## We found our archive, move it to the build directory.
-  cp ../${PS3LIBS}.tar.xz .
-fi
-
-
-## Unpack the source code.
-rm -Rf ${PS3LIBS} && tar -xvJf ${PS3LIBS}.tar.xz
+## Clone the source code from our github repository
+git clone https://github.com/Spork-Schivago/${PS3LIBS}
 
 ## Create the build directory.
 cd ${PS3LIBS}
